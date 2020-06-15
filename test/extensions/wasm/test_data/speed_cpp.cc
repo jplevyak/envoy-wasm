4530f7983fae46b979c72b098369ffd43b679cbb
Aakash Shukla <aakashshukla@google.com>
2020-06-15 14:42:45 +0000

    need to add stats proto, but having proto linking issues

--- test/extensions/wasm/test_data/speed_cpp.cc
+++ test/extensions/wasm/test_data/speed_cpp.cc
@@ -1,8 +1,8 @@
 // NOLINT(namespace-envoy)
 #include <string>
-
+#include <google/protobuf/util/json_util.h>
 #ifndef NULL_PLUGIN
-#include "proxy_wasm_intrinsics_lite.h"
+#include "proxy_wasm_intrinsics_full.h"
 // Required Proxy-Wasm ABI version.
 extern "C" PROXY_WASM_KEEPALIVE void proxy_abi_version_0_1_0() {}
 #else
@@ -10,6 +10,8 @@ extern "C" PROXY_WASM_KEEPALIVE void proxy_abi_version_0_1_0() {}
 using envoy::config::core::v3::GrpcService;
 #include "include/proxy-wasm/null_plugin.h"
 #endif
+
+// #include "source/extensions/common/wasm/declare_property.pb.h"
 
 START_WASM_PLUGIN(WasmSpeedCpp)
 
@@ -69,6 +71,46 @@ void grpc_service1000_test() {
   }
 }
 
+void modify_metadata_test() {
+  auto path = getRequestHeader(":path");
+  addRequestHeader("newheader", "newheadervalue");
+  auto server = getRequestHeader("server");
+  replaceRequestHeader("server", "envoy-wasm");
+  replaceRequestHeader("envoy-wasm", "server");
+  removeRequestHeader("newheader");
+}
+
+void modify_metadata1000_test() {
+  for (int x = 0; x < 1000; x++) {
+    auto path = getRequestHeader(":path");
+    addRequestHeader("newheader", "newheadervalue");
+    auto server = getRequestHeader("server");
+    replaceRequestHeader("server", "envoy-wasm");
+    replaceRequestHeader("envoy-wasm", "server");
+    removeRequestHeader("newheader");
+  }
+}
+
+void json_serialize_test() {
+  const std::string configuration = R"EOF(
+  "NAME": "example",
+  "READONLY":true
+  )EOF";
+  // envoy::source::extensions::common::wasm::DeclarePropertyArguments args;
+  GrpcService args;
+  google::protobuf::util::JsonStringToMessage(configuration, &args);
+}
+
+void json_deserialize_test() {
+  std::string json;
+  std::string value = "foo";
+  GrpcService grpc_service;
+
+  grpc_service.mutable_envoy_grpc()->set_cluster_name(value);
+  google::protobuf::util::MessageToJsonString(grpc_service, &json);
+
+}
+
 WASM_EXPORT(uint32_t, proxy_on_vm_start, (uint32_t, uint32_t configuration_size)) {
   const char* configuration_ptr = nullptr;
   size_t size;
@@ -89,6 +131,14 @@ WASM_EXPORT(uint32_t, proxy_on_vm_start, (uint32_t, uint32_t configuration_size)
     test_fn = &grpc_service_test;
   } else if (configuration == "grpc_service1000") {
     test_fn = &grpc_service1000_test;
+  } else if (configuration == "modify_metadata") {
+    test_fn = &modify_metadata_test;
+  } else if (configuration == "modify_metadata1000") {
+    test_fn = &modify_metadata1000_test;
+  } else if (configuration == "json_serialize") {
+    test_fn = &json_serialize_test;
+  } else if (configuration == "json_deserialize") {
+    test_fn = &json_deserialize_test;
   } else {
     std::string message = "on_start " + configuration;
     proxy_log(LogLevel::info, message.c_str(), message.size());
